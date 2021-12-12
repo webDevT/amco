c5145cf9b036940e31f6c228c8b8ee7ab7dcb240
karinadesyatnik <karinadesyatnik27@gmail.com>
2021-12-12 14:36:02 +0300

    marine

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,219 @@
 $(function(){
+
+
+
+
+if (window.location.hash.indexOf('fullcynthetic') !== -1) {
+   $('.tab-item').hide();
+    $('#fullcynthetic').show();
+  
+}  
+if (window.location.hash.indexOf('synthetic-technology') !== -1) {
+   $('.tab-item').hide();
+    $('#synthetic-technology').show();
+  
+}
+if (window.location.hash.indexOf('semi-synthetics') !== -1) {
+   $('.tab-item').hide();
+    $('#semi-synthetics').show();
+  
+}
+if (window.location.hash.indexOf('mineral') !== -1) {
+   $('.tab-item').hide();
+    $('#mineral').show();
+}
+if (window.location.hash.indexOf('atf') !== -1) {
+   $('.tab-item').hide();
+    $('#atf').show();
+}
+if (window.location.hash.indexOf('uhpx-series') !== -1) {
+   $('.tab-item').hide();
+    $('#uhpx-series').show();
+}
+if (window.location.hash.indexOf('xhp-series') !== -1) {
+   $('.tab-item').hide();
+    $('#xhp-series').show();
+}
+if (window.location.hash.indexOf('uhpd-series') !== -1) {
+   $('.tab-item').hide();
+    $('#uhpd-series').show();
+}
+if (window.location.hash.indexOf('shpd') !== -1) {
+   $('.tab-item').hide();
+    $('#shpd').show();
+}
+if (window.location.hash.indexOf('hd') !== -1) {
+   $('.tab-item').hide();
+    $('#hd').show();
+}
+if (window.location.hash.indexOf('igneus-series') !== -1) {
+   $('.tab-item').hide();
+    $('#igneus-series').show();
+}
+if (window.location.hash.indexOf('mo') !== -1) {
+   $('.tab-item').hide();
+    $('#mo').show();
+}
+if (window.location.hash.indexOf('mtf') !== -1) {
+   $('.tab-item').hide();
+    $('#mtf').show();
+}
+if (window.location.hash.indexOf('manual-transmission-full-syntethetics') !== -1) {
+   $('.tab-item').hide();
+    $('#manual-transmission-full-syntethetics').show();
+}
+if (window.location.hash.indexOf('industrial-gear-oils') !== -1) {
+   $('.tab-item').hide();
+    $('#industrial-gear-oils').show();
+}
+if (window.location.hash.indexOf('industrial-hydraulics') !== -1) {
+   $('.tab-item').hide();
+    $('#industrial-hydraulics').show();
+}
+if (window.location.hash.indexOf('aw') !== -1) {
+   $('.tab-item').hide();
+    $('#aw').show();
+}
+if (window.location.hash.indexOf('hvi') !== -1) {
+   $('.tab-item').hide();
+    $('#hvi').show();
+}
+if (window.location.hash.indexOf('compressor-oils') !== -1) {
+   $('.tab-item').hide();
+    $('#compressor-oils').show();
+}
+if (window.location.hash.indexOf('turbine-oils') !== -1) {
+   $('.tab-item').hide();
+    $('#turbine-oils').show();
+}
+if (window.location.hash.indexOf('outboard') !== -1) {
+   $('.tab-item').hide();
+    $('#outboard').show();
+}
+if (window.location.hash.indexOf('mco') !== -1) {
+   $('.tab-item').hide();
+    $('#mco').show();
+}
+if (window.location.hash.indexOf('sym') !== -1) {
+   $('.tab-item').hide();
+    $('#sym').show();
+}
+if (window.location.hash.indexOf('tpeo') !== -1) {
+   $('.tab-item').hide();
+    $('#tpeo').show();
+}
+
+
+
+
+$('a[href="product-info.html#fullcynthetic"]').click(function() {
+	 $('.tab-item').hide();
+    $('#fullcynthetic').show();
+})
+$('a[href="product-info.html#synthetic-technology"]').click(function() {
+	 $('.tab-item').hide();
+    $('#synthetic-technology').show();
+})
+$('a[href="product-info.html#semi-synthetics"]').click(function() {
+	 $('.tab-item').hide();
+    $('#semi-synthetics').show();
+})
+$('a[href="product-info.html#mineral"]').click(function() {
+	 $('.tab-item').hide();
+    $('#mineral').show();
+})
+$('a[href="product-info.html#atf"]').click(function() {
+	 $('.tab-item').hide();
+    $('#atf').show();
+})
+$('a[href="product-info.html#uhpx-series"]').click(function() {
+	 $('.tab-item').hide();
+    $('#uhpx-series').show();
+})
+$('a[href="product-info.html#xhp-series"]').click(function() {
+	 $('.tab-item').hide();
+    $('#xhp-series').show();
+})
+$('a[href="product-info.html#uhpd-series"]').click(function() {
+	 $('.tab-item').hide();
+    $('#uhpd-series').show();
+})
+$('a[href="product-info.html#shpd"]').click(function() {
+	 $('.tab-item').hide();
+    $('#shpd').show();
+})
+$('a[href="product-info.html#hd"]').click(function() {
+	 $('.tab-item').hide();
+    $('#hd').show();
+})
+$('a[href="product-info.html#igneus-series"]').click(function() {
+	 $('.tab-item').hide();
+    $('#igneus-series').show();
+})
+$('a[href="product-info.html#mo"]').click(function() {
+	 $('.tab-item').hide();
+    $('#mo').show();
+})
+$('a[href="product-info.html#mtf"]').click(function() {
+	 $('.tab-item').hide();
+    $('#mtf').show();
+})
+$('a[href="product-info.html#manual-transmission-full-syntethetics"]').click(function() {
+	 $('.tab-item').hide();
+    $('#manual-transmission-full-syntethetics').show();
+})
+$('a[href="product-info.html#industrial-gear-oils"]').click(function() {
+	 $('.tab-item').hide();
+    $('#industrial-gear-oils').show();
+})
+$('a[href="product-info.html#industrial-hydraulics"]').click(function() {
+	 $('.tab-item').hide();
+    $('#industrial-hydraulics').show();
+})
+$('a[href="product-info.html#aw"]').click(function() {
+	 $('.tab-item').hide();
+    $('#aw').show();
+})
+$('a[href="product-info.html#hvi"]').click(function() {
+	 $('.tab-item').hide();
+    $('#hvi').show();
+})
+
+$('a[href="product-info.html#compressor-oils"]').click(function() {
+	 $('.tab-item').hide();
+    $('#compressor-oils').show();
+})
+
+$('a[href="product-info.html#turbine-oils"]').click(function() {
+	 $('.tab-item').hide();
+    $('#turbine-oils').show();
+})
+
+$('a[href="product-info.html#outboard"]').click(function() {
+	 $('.tab-item').hide();
+    $('#outboard').show();
+})
+
+
+$('a[href="product-info.html#mco"]').click(function() {
+	 $('.tab-item').hide();
+    $('#mco').show();
+})
+
+$('a[href="product-info.html#sym"]').click(function() {
+	 $('.tab-item').hide();
+    $('#sym').show();
+})
+
+$('a[href="product-info.html#tpeo"]').click(function() {
+	 $('.tab-item').hide();
+    $('#tpeo').show();
+})
+
+
+
+
+
  if($(window).width() > 992) {
 $('.product-card').click(function(){
 	$('.product-card').removeClass('active');
@@ -13,6 +228,13 @@ $('.product-card').click(function(){
 $('.menu-btn').click(function(){
 	$('.menu').toggleClass('active');
 	$(this).toggleClass('active');
+	$('.sub-menu').removeClass('active');
+	$('.sub-sub-menu').removeClass('active');
+})
+
+$('.sub-sub-menu .menu-link').click(function(){
+	$('.menu').removeClass('active');
+	$('.menu-btn').removeClass('active');
 	$('.sub-menu').removeClass('active');
 	$('.sub-sub-menu').removeClass('active');
 })
